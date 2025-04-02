001b074617d695ac4f80f46f7235520e1eee2cb1
Gabriel Lima <gabriel.lima@speedbird.aero>
2025-04-02 10:06:29 -0300

    senac

---
+++ aula2/nome.js
@@ -0,0 +1,5 @@
+let nome = "Gabriel";
+const idade = 22;
+var ativo = true;
+
+console.log(nome, idade, ativo);
---
+++ aula3/if.js
@@ -0,0 +1,5 @@
+// Exemplo de if
+let numero = parseInt(prompt("Digite um número:"));
+if (numero > 5) {
+    console.log("O número é maior que 5");
+}
